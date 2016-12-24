20dafdc6e9761507f22a343e0fe207cd3f277881
Robin Philip Thomas <robinthoms123@gmail.com>
2016-12-24 21:31:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,26 +34,48 @@ restService.post('/hook', function (req, res) {
         console.log('result: ', speech);
 
         return res.json({
-            speech: speech,
+            "speech": speech,
             displayText: speech,
             source: 'apiai-webhook-sample',
-			
-			  "facebook": { 
-			  },
-			  "kik": [{
-			  }],
-			  "slack": {
-			  	"attachments": [
-					        {
-					            "title": "Title",
-					            "pretext": "Pretext _supports_ mrkdwn",
-					            "text": "Testing *right now!*",
-					            "mrkdwn_in": ["text", "pretext"]
-					        }
-					    ]
-			  },
-			  "telegram": {
-			  }
+			slack: {
+    				"text": "Would you like to play a game?",
+    			"attachments": [
+       		 {
+            "text": "Choose a game to play",
+            "fallback": "You are unable to choose a game",
+            "callback_id": "wopr_game",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "chess",
+                    "text": "Chess",
+                    "type": "button",
+                    "value": "chess"
+                },
+                {
+                    "name": "maze",
+                    "text": "Falken's Maze",
+                    "type": "button",
+                    "value": "maze"
+                },
+                {
+                    "name": "war",
+                    "text": "Thermonuclear War",
+                    "style": "danger",
+                    "type": "button",
+                    "value": "war",
+                    "confirm": {
+                        "title": "Are you sure?",
+                        "text": "Wouldn't you prefer a good game of chess?",
+                        "ok_text": "Yes",
+                        "dismiss_text": "No"
+                    }
+                }
+            ]
+        }
+    ]
+}
 			
         });
 
