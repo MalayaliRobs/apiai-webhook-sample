d2a3c5bacb823eeb918eb7a263bda65a69e51ddd
Robin Philip Thomas <robinthoms123@gmail.com>
2016-12-27 13:16:18 +0530

    postgres1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 var pg = require('pg');
-pg.defaults.ssl = true;
+pg.defaults.ssl = false;
 var conString = 'postgres://jiadkawgponomn:803cec759efcbd383bbd2ecd4d02de800ba073a90079b23f5b247a878afe85c1@ec2-54-221-212-48.compute-1.amazonaws.com:5432/dei1e9mld85lk9';
 
 const restService = express();
@@ -27,7 +27,7 @@ restService.post('/hook', function (req, res) {
                 {
                 	var name=requestBody.result.parameters['given-name'];
 
-                	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+                	pg.connect(conString, function(err, client, done) {
 				    
 				    console.log("connected to database");
 				    client.query('SELECT * FROM ajcestudents', function(err, result) {
