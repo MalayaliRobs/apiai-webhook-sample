6481c3f8057a245b69340509d5d95569d8d67f5f
Robin Philip Thomas <robinthoms123@gmail.com>
2017-03-15 13:54:12 +0530

    'multiplerows'

--- index.js
+++ index.js
@@ -48,10 +48,7 @@ restService.post('/hook', function (req, res) {
 					    done();
 					    console.log(result.rows[0].student_name);
 					     name1=result.rows[0].student_name;
-					     if(result.rows[1].student_name)
-					     {
-					     	name1="find row 2";
-					     }
+
 
 					     if (requestBody.result.fulfillment) 
 		                	{
