1c7c4b550bdf85d0fcb912b334e78c08a6b2ce2e
Robin Philip Thomas <robinthoms123@gmail.com>
2017-03-23 13:48:27 +0530

    multiple_result

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ restService.use(bodyParser.json());
 
 restService.post('/hook', function (req, res) {
 
-    console.log('hook request ');
+    console.log('hook request');
 
     try {
         var speech = 'empty speech';
